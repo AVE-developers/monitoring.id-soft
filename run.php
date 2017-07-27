94b94eb1f2c1067a5e72d7e9c77a874cc7aefeaa
Anton Rastyazhenko <rastyazhenko.a.v@366.ru>
2017-07-27 07:07:08 +0300

    Initial commit

--- run.php
+++ run.php
@@ -1,4 +1,4 @@
 <?php
-require_once realpath(__DIR__ . '/src/rb.php');
-require_once realpath(__DIR__ . '/src/index.inc');
+require_once __DIR__ . '/src/rb.php';
+require_once __DIR__ . '/src/index.inc';
 $app->run();
