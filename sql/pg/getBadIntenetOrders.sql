3f38d51f9ef2b99b1db066fe32e9ca80c29d657d
Anton Rastyazhenko <rastyazhenko.a.v@366.ru>
2017-07-28 18:05:41 +0300

    commit

--- sql/pg/getBadIntenetOrders.sql
+++ sql/pg/getBadIntenetOrders.sql
@@ -1,32 +1,28 @@
-WITH orders AS (
-    SELECT
-      public._document7084._number :: INTEGER        AS ndoc,
-      -- Номер документа
-      public._document7084._date_time :: TIMESTAMP   AS ddoc,
-      -- Дата документа
-      public._reference22._code :: SMALLINT          AS idapt,
-      public._document7084._Fld9649 :: VARCHAR(20)    AS int_ord_num
-    -- Код аптеки
-    FROM public._document7084
-      LEFT JOIN public._reference22
-      -- местаХранения
-        ON public._reference22._idrref = public._document7084._Fld7261RRef
-    WHERE public._document7084._fld7268rref = E'\\x80B17C208209698D4D847B33CBB4670B'
-          -- ТипНакладной: Интернет-заказ (_enum2660)
-          AND public._document7084._marked = FALSE -- Не помеченные на удаление
-          AND public._document7084._date_time < now() - '1 hours' :: INTERVAL
-          -- Документы, созданные более часа назад
-          AND public._document7084._Fld7264RRef = E'\\x82CBD37640B3337E4C90036947A739E4'
-          -- СтатусОфиса: Выгружен в аптеку (_enum7097)
-          AND (public._document7084._Fld7265RRef = E'\\x8A38CB71495F580F4D46B293AA0ADE7C'
-               -- СтатусАптеки: Неопределено (_Enum111)
-               OR public._document7084._Fld7265RRef =
-                  E'\\x00000000000000000000000000000000') -- СтатусАптеки: ПустоеЗначение (_Enum111)
-    ORDER BY public._document7084._date_time DESC
-)
 SELECT
-  a.ndoc :: INTEGER                                      AS ndoc,
-  extract(epoch from a.ddoc at time zone 'Europe/Moscow')AS ddoc,
-  a.idapt :: SMALLINT                                    AS idapt,
-  a.int_ord_num :: VARCHAR(20) AS int_ord_num
-FROM orders a
+  prihod._number :: INTEGER                                                    AS НомерПрихода,
+  -- Номер документа Документ.УЗ_ПоступлениеТовара
+  extract(EPOCH FROM prihod._date_time AT TIME ZONE 'Europe/Moscow') :: BIGINT AS ДатаПрихода,
+  -- Дата документа Документ.УЗ_ПоступлениеТовара
+  apteki._code :: SMALLINT                                                     AS НомерАптеки,
+  -- Код аптеки Справочник.МестаХранения
+  prihod._Fld9649                                                              AS НомерИнтернетЗаказа
+-- Номер интернет-заказа
+
+FROM public._document7084 prihod
+  LEFT OUTER JOIN public._reference22 apteki
+  -- Справочник.МестаХранения
+    ON prihod._Fld7261RRef = apteki._idrref
+
+WHERE prihod._fld7268rref = E'\\x80B17C208209698D4D847B33CBB4670B'
+      -- ТипНакладной: Интернет-заказ (_enum2660)
+      AND prihod._marked = FALSE -- Не помеченные на удаление
+      AND prihod._date_time < now() - '1 hours' :: INTERVAL
+      -- Документы, созданные более часа назад
+      AND prihod._Fld7264RRef = E'\\x82CBD37640B3337E4C90036947A739E4'
+      -- СтатусОфиса: Выгружен в аптеку (_enum7097)
+      AND (prihod._Fld7265RRef = E'\\x8A38CB71495F580F4D46B293AA0ADE7C'
+           -- СтатусАптеки: Неопределено (_Enum111)
+           OR prihod._Fld7265RRef =
+              E'\\x00000000000000000000000000000000'
+        -- СтатусАптеки: ПустоеЗначение (_Enum111)
+      )
--- sql/pg/getBadSuppErrors.sql
+++ sql/pg/getBadSuppErrors.sql
@@ -1,45 +1,52 @@
-WITH orders AS (
-    SELECT
-      public._document7084._number :: INTEGER        AS ndoc,
-      -- Номер документа
-      public._document7084._date_time :: TIMESTAMPTZ AS ddoc,
-      -- Дата документа
-      public._reference22._code :: SMALLINT          AS idapt
-    -- Код аптеки
-    FROM public._document7084
-      LEFT JOIN public._reference22
-      -- местаХранения
-        ON public._reference22._idrref = public._document7084._Fld7261RRef
-    WHERE public._document7084._fld7268rref != E'\\x80B17C208209698D4D847B33CBB4670B'
-          -- ТипНакладной: Интернет-заказ (_enum2660)
-          AND public._document7084._marked = FALSE -- Не помеченные на удаление
-          AND public._document7084._date_time < now() - '1 hours' :: INTERVAL
-          -- Документы, созданные более часа назад
-          AND public._document7084._date_time > now() - '720 hours' :: INTERVAL
-        -- Документы, моложе 720 часов
-          AND public._document7084._Fld7264RRef = E'\\x82CBD37640B3337E4C90036947A739E4'
-          -- СтатусОфиса: Выгружен в аптеку (_enum7097)
-          AND (public._document7084._Fld7265RRef = E'\\x8A38CB71495F580F4D46B293AA0ADE7C'
-               -- СтатусАптеки: Неопределено (_Enum111)
-               OR public._document7084._Fld7265RRef =
-                  E'\\x00000000000000000000000000000000') -- СтатусАптеки: ПустоеЗначение (_Enum111)
-    ORDER BY public._document7084._date_time DESC
-)
 SELECT
-  a.ndoc :: INTEGER                                      AS ndoc,
-  a.ddoc :: TIMESTAMPTZ                                  AS ddoc,
-  a.idapt :: SMALLINT                                    AS idapt,
-  EXTRACT(YEAR FROM a.ddoc) :: SMALLINT                  AS ddoc_year,
-  EXTRACT(MONTH FROM a.ddoc) :: SMALLINT                 AS ddoc_month,
-  EXTRACT(DAY FROM a.ddoc) :: SMALLINT                   AS ddoc_day,
-  EXTRACT(HOUR FROM a.ddoc) :: SMALLINT                  AS ddoc_hour,
-  EXTRACT(MINUTE FROM a.ddoc) :: SMALLINT                AS ddoc_minute,
+  prihod._number :: INTEGER                                                    AS НомерПрихода,
+  -- Номер документа Документ.УЗ_ПоступлениеТовара
+  extract(EPOCH FROM prihod._date_time AT TIME ZONE 'Europe/Moscow') :: BIGINT AS ДатаПрихода,
+  -- Дата документа Документ.УЗ_ПоступлениеТовара
+  apteki._code :: SMALLINT                                                     AS НомерАптеки,
+  -- Код аптеки Справочник.МестаХранения
+  prihod._Fld7249                                                              AS НомерВхПоставщика,
+  -- ВхНомерНакл	Документ.УЗ_ПоступлениеТовара.Реквизит.ВхНомерНакл
+  zepochkiKD._Fld9295                                                          AS НомерВхПоставщикаКД,
+  -- НомерВхПоставщика	РегистрСведений.ЦепочкиПоКроссДокингу.Ресурс.НомерВхПоставщика
+  postavshik._description                                                      AS ПоставщикНаименование,
+  -- Поставщик Наименовние
+  postavshikKD._description                                                    AS ПоставщикКдНаименование
+-- Поставщик Наименовние
 
-  EXTRACT(YEAR FROM c.mddoc :: TIMESTAMPTZ) :: SMALLINT  AS mddoc_year,
-  EXTRACT(MONTH FROM c.mddoc :: TIMESTAMPTZ) :: SMALLINT AS mddoc_month,
-  EXTRACT(DAY FROM c.mddoc :: TIMESTAMPTZ) :: SMALLINT   AS mddoc_day
+FROM public._document7084 prihod
 
-FROM orders a
-  LEFT JOIN (SELECT min(b.ddoc) :: TIMESTAMPTZ AS mddoc
-             FROM orders b) c ON a.ddoc = c.mddoc
-ORDER BY a.ddoc DESC
+  LEFT OUTER JOIN public._reference22 apteki
+  -- Справочник.МестаХранения
+    ON apteki._idrref = prihod._Fld7261RRef
+
+  LEFT OUTER JOIN public._inforg9292 zepochkiKD
+  -- цепочки по кросдокингу РегистрСведений.ЦепочкиПоКроссДокингу
+    ON prihod._idrref = zepochkiKD._fld9293rref
+
+  LEFT OUTER JOIN public._reference33 postavshik
+  -- Поставщик Справочник.Поставщики
+    ON prihod._fld7259rref = postavshik._idrref
+
+  LEFT OUTER JOIN public._reference33 postavshikKD
+  -- ПоставщикКД Справочник.Поставщики
+    ON prihod._fld9648rref = postavshikKD._idrref
+
+
+WHERE prihod._fld7268rref != E'\\x80B17C208209698D4D847B33CBB4670B'
+      -- ТипНакладной: Интернет-заказ (_enum2660)
+      AND prihod._marked = FALSE
+      -- Не помеченные на удаление
+      AND prihod._date_time < now() - '1 hours' :: INTERVAL
+      -- Документы, созданные более часа назад
+      AND prihod._date_time > now() - '720 hours' :: INTERVAL
+      -- Документы, моложе 720 часов
+      AND prihod._Fld7264RRef = E'\\x82CBD37640B3337E4C90036947A739E4'
+      -- СтатусОфиса: Выгружен в аптеку (_enum7097)
+      AND (prihod._Fld7265RRef = E'\\x8A38CB71495F580F4D46B293AA0ADE7C'
+           -- cтатус документа аптеки: Неопределено (_Enum111)
+           OR prihod._Fld7265RRef = E'\\x00000000000000000000000000000000'
+        -- cтатус документа аптеки: ПустоеЗначение (_Enum111)
+      )
+
+ORDER BY prihod._date_time DESC
